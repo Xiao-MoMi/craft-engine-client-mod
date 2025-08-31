8a61656db6a4db1f88b9bceeea343ade94400bcb
jhqwqmc <2110242767@qq.com>
2025-09-01 05:29:36 +0800

    refactor(network): 重构注册到注册表

--- fabric/src/main/java/net/momirealms/craftengine/fabric/network/NetworkManager.java
+++ fabric/src/main/java/net/momirealms/craftengine/fabric/network/NetworkManager.java
@@ -7,25 +7,25 @@ import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientConfigurationPacketListenerImpl;
+import net.minecraft.core.Holder;
+import net.minecraft.core.RegistrationInfo;
+import net.minecraft.core.WritableRegistry;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.block.Block;
 import net.momirealms.craftengine.fabric.CraftEngineFabricMod;
 import net.momirealms.craftengine.fabric.client.config.ModConfig;
-import net.momirealms.craftengine.fabric.network.protocol.CancelBlockUpdateData;
-import net.momirealms.craftengine.fabric.network.protocol.ClientBlockStateSizeData;
-import net.momirealms.craftengine.fabric.network.protocol.ClientCustomBlockData;
+import net.momirealms.craftengine.fabric.network.protocol.CancelBlockUpdatePacket;
+import net.momirealms.craftengine.fabric.network.protocol.ClientBlockStateSizePacket;
+import net.momirealms.craftengine.fabric.network.protocol.ClientCustomBlockPacket;
+import net.momirealms.craftengine.fabric.registries.BuiltInRegistries;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Optional;
 
 public class NetworkManager {
     public static boolean serverInstalled = false;
     private final CraftEngineFabricMod mod;
-    private static final Map<Class<Data>, Byte> classToType = new HashMap<>();
-    private static final Map<Byte, StreamCodec<FriendlyByteBuf, Data>> typeToCodec = new HashMap<>();
-    private static final AtomicInteger typeCounter = new AtomicInteger(0);
 
     public NetworkManager(CraftEngineFabricMod mod) {
         this.mod = mod;
@@ -38,50 +38,39 @@ public class NetworkManager {
     }
 
     private void registerDataTypes() {
-        registerDataType(ClientCustomBlockData.class, ClientCustomBlockData.CODEC);
-        registerDataType(CancelBlockUpdateData.class, CancelBlockUpdateData.CODEC);
-        registerDataType(ClientBlockStateSizeData.class, ClientBlockStateSizeData.CODEC);
+        registerDataType(ClientCustomBlockPacket.TYPE, ClientCustomBlockPacket.CODEC);
+        registerDataType(CancelBlockUpdatePacket.TYPE, CancelBlockUpdatePacket.CODEC);
+        registerDataType(ClientBlockStateSizePacket.TYPE, ClientBlockStateSizePacket.CODEC);
     }
 
-    @SuppressWarnings("unchecked")
-    private <T extends Data> void registerDataType(Class<T> dataClass, StreamCodec<FriendlyByteBuf, T> codec) {
-        if (classToType.containsKey(dataClass)) {
-            this.mod.logger().warn("Duplicate data type class: " + dataClass.getName());
-            return;
-        }
-        int next = typeCounter.getAndIncrement();
-        if (next > 255) {
-            throw new IllegalStateException("Too many data types registered, byte index overflow (max 256)");
-        }
-        byte type = (byte) next;
-        classToType.put((Class<Data>) dataClass, type);
-        typeToCodec.put(type, (StreamCodec<FriendlyByteBuf, Data>) codec);
+    public static <T extends ModPacket> void registerDataType(ResourceKey<StreamCodec<FriendlyByteBuf, ? extends ModPacket>> key, StreamCodec<FriendlyByteBuf, T> codec) {
+        ((WritableRegistry<StreamCodec<FriendlyByteBuf, ? extends ModPacket>>) BuiltInRegistries.MOD_PACKET).register(key, codec, RegistrationInfo.BUILT_IN);
     }
 
     private void initChannel(ClientConfigurationPacketListenerImpl handler, Minecraft client) {
-        sendData(new ClientBlockStateSizeData(Block.BLOCK_STATE_REGISTRY.size()));
+        sendData(new ClientBlockStateSizePacket(Block.BLOCK_STATE_REGISTRY.size()));
 
         if (!ModConfig.enableNetwork && !ModConfig.enableCancelBlockUpdate) {
             return;
         }
 
         if (ModConfig.enableNetwork) {
-            sendData(new ClientCustomBlockData(Block.BLOCK_STATE_REGISTRY.size()));
+            sendData(new ClientCustomBlockPacket(Block.BLOCK_STATE_REGISTRY.size()));
         } else {
-            sendData(new CancelBlockUpdateData(true));
+            sendData(new CancelBlockUpdatePacket(true));
         }
     }
 
-    public void sendData(Data data) {
-        Class<? extends Data> dataClass = data.getClass();
-        Byte type = classToType.get(dataClass);
-        if (type == null) {
-            CraftEngineFabricMod.instance().logger().warn("Unknown data type class: " + dataClass.getName());
+    public void sendData(ModPacket data) {
+        Optional<Holder.Reference<StreamCodec<FriendlyByteBuf, ? extends ModPacket>>> optionalType = BuiltInRegistries.MOD_PACKET.get(data.type());
+        if (optionalType.isEmpty()) {
+            this.mod.logger().warn("Unknown data type class: " + data.getClass().getName());
             return;
         }
-        StreamCodec<FriendlyByteBuf, Data> codec = typeToCodec.get(type);
+        @SuppressWarnings("unchecked")
+        StreamCodec<FriendlyByteBuf, ModPacket> codec = (StreamCodec<FriendlyByteBuf, ModPacket>) optionalType.get().value();
         FriendlyByteBuf buf = new FriendlyByteBuf(Unpooled.buffer());
-        buf.writeByte(type);
+        buf.writeByte(BuiltInRegistries.MOD_PACKET.getId(codec));
         codec.encode(buf, data);
         ClientConfigurationNetworking.send(new CraftEnginePayload(buf.array()));
     }
@@ -90,13 +79,14 @@ public class NetworkManager {
         byte[] data = payload.data();
         FriendlyByteBuf buf = new FriendlyByteBuf(Unpooled.wrappedBuffer(data));
         byte type = buf.readByte();
-        StreamCodec<FriendlyByteBuf, Data> codec = typeToCodec.get(type);
+        @SuppressWarnings("unchecked")
+        StreamCodec<FriendlyByteBuf, ModPacket> codec = (StreamCodec<FriendlyByteBuf, ModPacket>) BuiltInRegistries.MOD_PACKET.byId(type);
         if (codec == null) {
-            CraftEngineFabricMod.instance().logger().warn("Unknown data type received: " + type);
+            this.mod.logger().warn("Unknown data type received: " + type);
             return;
         }
 
-        Data networkData = codec.decode(buf);
+        ModPacket networkData = codec.decode(buf);
         networkData.handle(context);
     }
 }
