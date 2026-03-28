de25109a65b1ead160b6b8e70887c039448e1d55
jhqwqmc <jhcraft@qq.com>
2026-03-28 15:37:48 +0800

    实现预览配方输入物品显示数量并提升模组版本到26.4

---
+++ fabric/src/main/java/net/momirealms/craftengine/fabric/mixin/GhostSlotsMixin.java
@@ -0,0 +1,25 @@
+package net.momirealms.craftengine.fabric.mixin;
+
+import net.minecraft.client.gui.screens.recipebook.GhostSlots;
+import net.momirealms.craftengine.fabric.config.ModConfig;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(GhostSlots.class)
+public class GhostSlotsMixin {
+
+    @Redirect(
+            method = "method_62030",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/client/gui/screens/recipebook/GhostSlots$GhostSlot;isResultSlot:Z",
+                    opcode = Opcodes.GETFIELD,
+                    ordinal = 1
+            )
+    )
+    private boolean ce$modifyIsResultSlot(GhostSlots.GhostSlot instance) {
+        return ModConfig.INSTANCE.forceGhostRecipeShowInputItemStackCount() || instance.isResultSlot;
+    }
+}
