8b146297410f5f472b365c88abc375b1cc109170
jhqwqmc <jhcraft@qq.com>
2026-05-20 13:15:22 +0800

    重构项目

---
+++ fabric/src/main/java/net/momirealms/craftengine/fabric/network/protocol/ClientboundVisualBlockStateBatchFinishedPacket.java
@@ -0,0 +1,46 @@
+package net.momirealms.craftengine.fabric.network.protocol;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.resources.ResourceLocation;
+import net.momirealms.craftengine.fabric.block.BlockManager;
+import net.momirealms.craftengine.fabric.network.ClientCustomPacket;
+import net.momirealms.craftengine.fabric.network.Context;
+import org.jetbrains.annotations.NotNull;
+
+@Environment(EnvType.CLIENT)
+public final class ClientboundVisualBlockStateBatchFinishedPacket implements ClientCustomPacket {
+    public static final ClientboundVisualBlockStateBatchFinishedPacket INSTANCE = new ClientboundVisualBlockStateBatchFinishedPacket();
+    public static final ResourceLocation ID = ResourceLocation.fromNamespaceAndPath("craftengine", "visual_block_state_batch_finished");
+    public static final Type<ClientboundVisualBlockStateBatchFinishedPacket> TYPE = new Type<>(ID);
+    public static final StreamCodec<FriendlyByteBuf, ClientboundVisualBlockStateBatchFinishedPacket> CODEC = ClientCustomPacket.codec(
+            ($, $$) -> {
+            },
+            $ -> INSTANCE
+    );
+
+    private ClientboundVisualBlockStateBatchFinishedPacket() {
+    }
+
+    @Override
+    public ResourceLocation id() {
+        return ID;
+    }
+
+    @Override
+    public StreamCodec<FriendlyByteBuf, ClientboundVisualBlockStateBatchFinishedPacket> codec() {
+        return CODEC;
+    }
+
+    @Override
+    public @NotNull Type<ClientboundVisualBlockStateBatchFinishedPacket> type() {
+        return TYPE;
+    }
+
+    @Override
+    public void handle(Context context) {
+        BlockManager.instance().handleVisualBlockStateBatchFinished();
+    }
+}
--- fabric/src/main/java/net/momirealms/craftengine/fabric/registries/BuiltInRegistries.java
+++ fabric/src/main/java/net/momirealms/craftengine/fabric/registries/BuiltInRegistries.java
@@ -8,13 +8,15 @@ import net.minecraft.core.Registry;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.resources.ResourceKey;
-import net.momirealms.craftengine.fabric.network.ModPacket;
+import net.momirealms.craftengine.fabric.network.ClientCustomPacket;
+import net.momirealms.craftengine.fabric.network.ServerCustomPacket;
 
 @Environment(EnvType.CLIENT)
 public class BuiltInRegistries {
     private BuiltInRegistries() {}
 
-    public static final Registry<StreamCodec<FriendlyByteBuf, ? extends ModPacket>> MOD_PACKET = registerSimple(Registries.MOD_PACKET);
+    public static final Registry<StreamCodec<FriendlyByteBuf, ? extends ClientCustomPacket>> CLIENT_MOD_PACKET = registerSimple(Registries.CLIENT_MOD_PACKET);
+    public static final Registry<StreamCodec<FriendlyByteBuf, ? extends ServerCustomPacket>> SERVER_MOD_PACKET = registerSimple(Registries.SERVER_MOD_PACKET);
 
     private static <T> Registry<T> registerSimple(ResourceKey<? extends Registry<T>> resourceKey) {
         return new MappedRegistry<>(resourceKey, Lifecycle.stable(), false);
